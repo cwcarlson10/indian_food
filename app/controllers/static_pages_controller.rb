aa12b2a349c969b29e099ae80b8b82318ea1927d
Christopher Carlson <christophercarlson@Christophers-MacBook-Pro-2.local>
2016-07-16 15:59:11 -0600

    generated the static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def menu
+  end
+
+  def about
+  end
+
+  def photos
+  end
+
+  def story
+  end
+
+  def catering
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/menu'
+
+  get 'static_pages/about'
+
+  get 'static_pages/photos'
+
+  get 'static_pages/story'
+
+  get 'static_pages/catering'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
